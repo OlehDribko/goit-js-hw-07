dcefd260855775b0f74cf7b65d6db922acfdb918
OlehDribko <131261254+OlehDribko@users.noreply.github.com>
2023-12-04 23:25:03 +0200

    first homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const jsContainer = document.querySelector('.gallery');
+
+const imgPush = createGalerymarcup(galleryItems);
+jsContainer.insertAdjacentHTML('beforeend', imgPush)
+
+
+
+function createGalerymarcup(items) {
+    return items
+        .map(({ original, preview, description }) => 
+         `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    ).join("")
+}
+
+jsContainer.addEventListener('click', (event) => {
+     event.preventDefault()
+    
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    
+    console.log(event.target.src)
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.src}" width="800" height="600">
+    </div>
+`)
+    instance.show();
+    jsContainer.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    })
+
+});
+
+
+
+    
+
+    
